04bc9e31e6cd7c40092d66fc0905fa2d9b846741
Ab <Ab@DESKTOP-4F514TS>
2020-09-26 21:19:19 +0500

    Updating Expanditure Model file

--- Models/ExpenditureType.cs
+++ Models/ExpenditureType.cs
@@ -1,8 +1,12 @@
+using System;
+
 namespace ExpenditureApi.Models
 {
     public class ExpenditureType
     {
         public int Id { get; set; }
         public string Type { get; set; }
+        public DateTime CreationDate {get;set;}
+
     }
 }
