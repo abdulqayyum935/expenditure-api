a1abd44789be69d8b366f81ba89de6a5897239ab
Ab <Ab@DESKTOP-4F514TS>
2020-09-26 19:37:52 +0500

    test comment added in Income.cs

--- Models/Income.cs
+++ Models/Income.cs
@@ -5,6 +5,7 @@ namespace ExpenditureApi.Models
     public class Income
     {
         //Auto increment & primary key in the table
+        // test comment
         public int Id {get;set;}
         [Column(TypeName = "decimal(18,2)")]
         public decimal Amount { get; set; }
