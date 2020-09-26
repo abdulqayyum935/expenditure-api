408047b176c3ff0ad2acccc0a29c3a1596fa15c9
Uzair khan <pk.uzikhan@gmail.com>
2020-09-26 20:56:05 +0500

    commts removes

--- Models/Income.cs
+++ Models/Income.cs
@@ -5,7 +5,6 @@ namespace ExpenditureApi.Models
     public class Income
     {
         //Auto increment & primary key in the table
-        // test comment
         public int Id {get;set;}
         [Column(TypeName = "decimal(18,2)")]
         public decimal Amount { get; set; }
